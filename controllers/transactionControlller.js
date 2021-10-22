cd2b9957d37dadcf80f345f481e915172a428b29
reyhananda <reyhanandasetiawan@gmail.com>
2021-10-22 09:44:52 +0700

    User Transaction As Admin

--- controllers/transactionControlller.js
+++ controllers/transactionControlller.js
@@ -30,4 +30,15 @@ module.exports = {
       res.status(200).send(result);
     });
   },
+  getAllTransaction: (req, res) => {
+    let AllTransactionsQuery = `SELECT idorder, user.full_name, db_farmasi1.order.order_status, db_farmasi1.order.order_date, 
+    db_farmasi1.order.total_item, db_farmasi1.order.order_price, shipping.address
+    FROM db_farmasi1.order JOIN user ON db_farmasi1.order.iduser = user.iduser 
+    JOIN shipping ON db_farmasi1.order.idshipping= shipping.idshipping;`;
+
+    db.query(AllTransactionsQuery, (err, results) => {
+      if (err) return res.status(500).send(err);
+      res.status(200).send(results);
+    });
+  },
 };
--- routers/transactionRouter.js
+++ routers/transactionRouter.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const {transactionController} = require('../controllers')
-const routers = express.Router()
+const express = require("express");
+const { transactionController } = require("../controllers");
+const routers = express.Router();
 
-routers.get('/get-history', transactionController.getTransactionHistory)
-routers.get('/get-detail', transactionController.detailTransactionHistory)
-
-module.exports = routers
+routers.get("/get-history", transactionController.getTransactionHistory);
+routers.get("/get-detail", transactionController.detailTransactionHistory);
+routers.get("/get-transaction", transactionController.getAllTransaction);
+module.exports = routers;
