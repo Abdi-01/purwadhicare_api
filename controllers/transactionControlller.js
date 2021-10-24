86eaad4f94ff7d86ee67335652773084504e20f6
reyhananda <reyhanandasetiawan@gmail.com>
2021-10-24 21:14:35 +0700

    BE-REY-018 : Add User Transaction, & Detail Transactions For Recipe Order

--- controllers/transactionControlller.js
+++ controllers/transactionControlller.js
@@ -41,4 +41,18 @@ module.exports = {
       res.status(200).send(results);
     });
   },
+  getDetailTransaction: (req, res) => {
+    let DetailTransaction = `Select u.full_name, od.idorder, p.product_name, p.price_unit, p.price_stock, od.quantity, s.idshipping, s.address, order_price, order_status, order_date, total_item, payment_image , recipe_image 
+    FROM db_farmasi1.order_detail as od 
+    JOIN db_farmasi1.order  o ON od.idorder = o.idorder 
+    JOIN product p ON od.idproduct = p.idproduct 
+    JOIN shipping as s on s.idshipping = o.idshipping 
+    JOIN user as u on u.iduser = o.iduser
+    where od.idorder =  ${req.params.id};`;
+
+    db.query(DetailTransaction, (err, results) => {
+      if (err) return res.status(500).send(err);
+      res.status(200).send(results);
+    });
+  },
 };
